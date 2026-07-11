adb3aa3ea35c5472410ccd57c47e2d0f7ef77d0b
Prince Keshri <prince12345keshri@gmail.com>
2026-07-11 22:09:31 +0530

    Replace firebase config values with placeholders
    Patched security flaw

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,13 +1,13 @@
 export const API_BASE_URL = "http://127.0.0.1:8000/api/v1";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyB1Ou-dm8fDMkWp5vCTJqsFv0Px-bH9qE8",
-    authDomain: "janmitra-1be2a.firebaseapp.com",
-    projectId: "janmitra-1be2a",
-    storageBucket: "janmitra-1be2a.firebasestorage.app",
-    messagingSenderId: "1095900364063",
-    appId: "1:1095900364063:web:3b555327edaf9b0233c42a",
-    measurementId: "G-023BC4H2DF"
+    apiKey: "<firebase_api>",
+    authDomain: "<firebase_auth_domain>",
+    projectId: "<firebase_project_id>",
+    storageBucket: "<firebase_project_bucket>",
+    messagingSenderId: "<firebase_id>",
+    appId: "<firebase_app_id>",
+    measurementId: "<firebase_measurement_id>"
 };
 
 
